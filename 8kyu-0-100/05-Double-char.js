c8387f6efd9a7959a4e3f4b820fab9aa54aa0de5
Nikita Alpatov <nikitaa95@mail.ru>
2023-08-04 12:21:14 +0300

    08kyu 05 Double char

---
+++ 8kyu-0-100/05-Double-char.js
@@ -0,0 +1,19 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+//
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+
+function doubleChar(str) {
+    return str.split("").map(c => c+c).join('')
+}
+
+//function doubleChar(str) {
+//   let newStr = ''
+//   for (let i = 0; i < str.length; i++){
+//     newStr += (str[i]+str[i])
+//   }
+//   return newStr
+// }
